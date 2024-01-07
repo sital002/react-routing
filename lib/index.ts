11b56c4bff5900d4d67a58ff6684cfa6d2df5ff7
sital002 <93700622+sital002@users.noreply.github.com>
2024-01-07 21:58:30 +0545

    remoed test funtion helloWorld

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,5 @@ import BrowserRouter from "./BrowserRouter";
 import Routes from "./Routes";
 import Route from "./Route";
 import Link from "./Link";
-function helloWorld(name: string) {
-  console.log("Hello World", name);
-}
-export { BrowserRouter, Routes, Route, helloWorld, Link };
+
+export { BrowserRouter, Routes, Route, Link };
