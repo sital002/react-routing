eb3362487adbbd581d4a62bcdaa84cf6bc08ff63
sital002 <93700622+sital002@users.noreply.github.com>
2024-01-08 15:00:47 +0545

    added  useRouter hook

---
+++ lib/hooks/useRouter.ts
@@ -0,0 +1,26 @@
+import React from "react";
+import { RouterContext } from "../BrowserRouter";
+
+function useRouter() {
+  const context = React.useContext(RouterContext);
+  if (context === null)
+    throw new Error("useRouter must be used inside Browser");
+
+  const router = {
+    push: (path: string) => {
+      if (typeof path !== "string") throw new Error("Path must be string");
+      window.history.pushState(path, path, path);
+      window.dispatchEvent(new Event("popstate"));
+    },
+    forward: () => {
+      window.history.forward();
+    },
+    back: () => {
+      window.history.back();
+    },
+  };
+
+  return router;
+}
+
+export default useRouter;
--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,6 @@ import BrowserRouter from "./BrowserRouter";
 import Routes from "./Routes";
 import Route from "./Route";
 import Link from "./Link";
+import useRouter from "./hooks/useRouter";
 
-export { BrowserRouter, Routes, Route, Link };
+export { BrowserRouter, Routes, Route, Link, useRouter };
